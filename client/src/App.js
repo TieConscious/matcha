1e9b69b6d2104c084826adede5337c7988f0dc04
CptBang <muashims@gmail.com>
2019-07-19 21:17:00 -0700

    not working version of app

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import AppNavbar from './component/AppNavbar';
+import './App.css';
+
+import { Container } from 'reactstrap';
+
+class App extends Component {
+  render() {
+    return (
+     <AppNavbar />
+    );
+  }
+}
+
+export default App;
