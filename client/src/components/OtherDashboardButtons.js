22040d3208a244c83a1c93266af9c42eb7edc34b
CptBang <muashims@gmail.com>
2019-09-04 14:29:52 -0700

    gonna pull

--- client/src/components/OtherDashboardButtons.js
+++ client/src/components/OtherDashboardButtons.js
@@ -8,7 +8,6 @@ import Button from "@material-ui/core/Button";
 import { updateLike } from "../actions/updateActions";
 import { updateFameRate } from "../actions/updateActions";
 
-
 const styles = {
   button: {
     margin: "5%"
@@ -19,12 +18,12 @@ class OtherDashboardButtons extends Component {
   static propTypes = {
     auth: PropTypes.object.isRequired
   };
-  
+
   constructor(props) {
-      super(props);
-      this.state = {
-          user: null,
-      }
+    super(props);
+    this.state = {
+      user: null
+    };
   }
   componentDidMount = () => {
     // If not logged in and user navigates to Dashboard page, should redirect them to landing page
@@ -32,8 +31,7 @@ class OtherDashboardButtons extends Component {
       this.props.history.push("/");
     }
     this.props.loadUser();
-    
-  }
+  };
 
   componentDidUpdate() {
     // If not logged in and user navigates to Dashboard page, should redirect them to landing page
@@ -45,72 +43,76 @@ class OtherDashboardButtons extends Component {
   handleLike = () => {
     this.props.updateLike(this.props.info._id, "like", this.props.user._id);
     this.props.updateFameRate(this.props.info._id, "like");
-  }
+  };
 
   handleDislike = () => {
     this.props.updateLike(this.props.info._id, "dislike", this.props.user._id);
     this.props.updateFameRate(this.props.info._id, "dislike");
-  }
+  };
 
   handleBlock = () => {
     this.props.updateLike(this.props.info._id, "block", this.props.user._id);
-  }
+  };
   handleReport = () => {
-    alert("User: " + this.props.info.firstname + " reported for being bad")
-  }
+    alert("User: " + this.props.info.firstname + " reported for being bad");
+  };
   render() {
     const { classes } = this.props;
     const displayLike = (
       <Button
-            className={classes.button}
-            onClick={this.handleLike}
-            variant="contained"
-            component="span"
-            color="primary"
-        >
-            LIKE
-        </Button>
+        className={classes.button}
+        onClick={this.handleLike}
+        variant="contained"
+        component="span"
+        color="primary"
+      >
+        LIKE
+      </Button>
     );
     const displayUnlike = (
       <Button
-            className={classes.button}
-            onClick={this.handleDislike}
-            variant="contained"
-            component="span"
-            color="primary"
-        >
-            UNLIKE
-        </Button>
-    )
+        className={classes.button}
+        onClick={this.handleDislike}
+        variant="contained"
+        component="span"
+        color="primary"
+      >
+        UNLIKE
+      </Button>
+    );
 
     const displayBlock = (
+      <Button
+        className={classes.button}
+        variant="contained"
+        component="span"
+        color="secondary"
+        onClick={this.handleBlock}
+      >
+        BLOCK
+      </Button>
+    );
+    return (
+      <Box className={classes.paper}>
+        {this.props.user.blocked.includes(this.props.info._id)
+          ? ""
+          : this.props.user.likes.includes(this.props.info._id)
+          ? displayUnlike
+          : displayLike}
+        {this.props.user.blocked.includes(this.props.info._id) ? (
+          <h2>USER IS BLOCKED</h2>
+        ) : (
+          displayBlock
+        )}
         <Button
           className={classes.button}
           variant="contained"
           component="span"
-          color="secondary"
-          onClick={this.handleBlock}
+          onClick={this.handleReport}
         >
-          BLOCK
-        </Button>
-    )
-    return (
-      <Box className={classes.paper}>
-        {this.props.user.blocked.includes(this.props.info._id) ? "" 
-        : 
-        this.props.user.likes.includes(this.props.info._id) ? displayUnlike : displayLike}
-        {this.props.user.blocked.includes(this.props.info._id) ? <h2>USER IS BLOCKED</h2> : displayBlock}
-        <Button
-            className={classes.button}
-            variant="contained"
-            component="span"
-            color="contained"
-            onClick={this.handleReport}
-        >
-            REPORT
+          REPORT
         </Button>
       </Box>
-      
     );
   }
 }
